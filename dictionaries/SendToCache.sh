2eabe226a839a0a4f69471087e91c4a489a5b12c
jwest <john.westbrook@rcsb.org>
2019-12-04 12:42:03 -0500

    Sync rcsb dictionary

--- dictionaries/SendToCache.sh
+++ dictionaries/SendToCache.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 #
-cp rcsb_mmcif_ext_v1.dic  ../../py-rcsb_db/CACHE/dictionaries/rcsb_mmcif_ext_v1.dic
+cp rcsb_mmcif_ext_base.dic  rcsb_mmcif_ext_v1.dic
 #
 cd DEPRECATED
-cat *.tdic >> ../../../py-rcsb_db/CACHE/dictionaries/rcsb_mmcif_ext_v1.dic
+cat *.tdic >> ../rcsb_mmcif_ext_v1.dic
 #
 cd ../ENTITY_TYPE_SPECIFIC_CATEGORIES
-cat *.tdic >> ../../../py-rcsb_db/CACHE/dictionaries/rcsb_mmcif_ext_v1.dic
+cat *.tdic >> ../rcsb_mmcif_ext_v1.dic
 #
 cd ../CONTAINER_IDENTIFIER_CATEGORIES
-cat *.tdic >> ../../../py-rcsb_db/CACHE/dictionaries/rcsb_mmcif_ext_v1.dic
+cat *.tdic >> ../rcsb_mmcif_ext_v1.dic
 #
 cd ../FEATURE_CATEGORIES
-cat *.tdic >> ../../../py-rcsb_db/CACHE/dictionaries/rcsb_mmcif_ext_v1.dic
+cat *.tdic >> ../rcsb_mmcif_ext_v1.dic
 #
+cd ..
+cp rcsb_mmcif_ext_v1.dic ../../py-rcsb_db/CACHE/dictionaries/rcsb_mmcif_ext_v1.dic
