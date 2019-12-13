ad44f9ea522eb05baabb3dc0ba9e9e6b8f68a411
jwest <john.westbrook@rcsb.org>
2019-12-13 14:21:38 -0500

    Major revision of repository_holdings collections and miscellaneous tweaks on core schema.

--- dictionaries/SendToCache.sh
+++ dictionaries/SendToCache.sh
@@ -14,5 +14,8 @@ cat *.tdic >> ../rcsb_mmcif_ext_v1.dic
 cd ../FEATURE_CATEGORIES
 cat *.tdic >> ../rcsb_mmcif_ext_v1.dic
 #
+cd ../REPOSITORY_HOLDINGS
+cat *.tdic >> ../rcsb_mmcif_ext_v1.dic
+#
 cd ..
 cp rcsb_mmcif_ext_v1.dic ../../py-rcsb_db/CACHE/dictionaries/rcsb_mmcif_ext_v1.dic
